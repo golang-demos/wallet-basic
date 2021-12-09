fe67986f32116a8dfa9ee291412855fed49693ea
Vinay Jeurkar <vjeurkar03@gmail.com>
2021-12-10 03:25:17 +0530

    Added order APIs placeholders

---
+++ api/controllers/order.go
@@ -0,0 +1,21 @@
+package controllers
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func orderListHandler(c *fiber.Ctx) error {
+	return nil
+}
+func orderDetailsHandler(c *fiber.Ctx) error {
+	return nil
+}
+func orderCreateHandler(c *fiber.Ctx) error {
+	return nil
+}
+func orderUpdateHandler(c *fiber.Ctx) error {
+	return nil
+}
+func orderDeleteHandler(c *fiber.Ctx) error {
+	return nil
+}
