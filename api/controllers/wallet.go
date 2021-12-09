a1ed995992a3e1ee4df3b47e0e9ad03ddfc9fbdc
Vinay Jeurkar <vjeurkar03@gmail.com>
2021-12-10 03:25:57 +0530

    Added wallet APIs placeholders

---
+++ api/controllers/wallet.go
@@ -0,0 +1,13 @@
+package controllers
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func walletDetailsHandler(c *fiber.Ctx) error {
+	return nil
+}
+
+func walletDepositHandler(c *fiber.Ctx) error {
+	return nil
+}
