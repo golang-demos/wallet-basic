ded1621b65c4bc29988f141811f01385329dd57a
Vinay Jeurkar <vjeurkar03@gmail.com>
2021-12-10 03:25:29 +0530

    Added session APIs placeholders

---
+++ api/controllers/session.go
@@ -0,0 +1,15 @@
+package controllers
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func sessionDetailsHandler(c *fiber.Ctx) error {
+	return nil
+}
+func sessionLoginHandler(c *fiber.Ctx) error {
+	return nil
+}
+func sessionLogoutHandler(c *fiber.Ctx) error {
+	return nil
+}
