648bff6f9c56aa70ccfefbd5fdf82351c7d76194
Vinay Jeurkar <vjeurkar03@gmail.com>
2021-12-10 03:25:43 +0530

    Added product APIs placeholders

---
+++ api/controllers/product.go
@@ -0,0 +1,21 @@
+package controllers
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func productListHandler(c *fiber.Ctx) error {
+	return nil
+}
+func productCreateHandler(c *fiber.Ctx) error {
+	return nil
+}
+func productDetailsHandler(c *fiber.Ctx) error {
+	return nil
+}
+func productUpdateHandler(c *fiber.Ctx) error {
+	return nil
+}
+func productDeleteHandler(c *fiber.Ctx) error {
+	return nil
+}
