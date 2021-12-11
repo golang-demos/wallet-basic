615a887f3712d7b4f8fa2aeafff4c7f88e167632
Vinay Jeurkar <vjeurkar03@gmail.com>
2021-12-12 02:24:16 +0530

    Custom response for session details

--- api/controllers/session.go
+++ api/controllers/session.go
@@ -6,7 +6,17 @@ import (
 
 func sessionDetailsHandler(c *fiber.Ctx) error {
 	loggedInUser := c.Locals("SessionUser")
-	return c.JSON(loggedInUser)
+	isLoggedIn := false
+	if loggedInUser != nil {
+		isLoggedIn = true
+	}
+	return c.JSON(&struct {
+		LoggedIn bool
+		User     interface{}
+	}{
+		LoggedIn: isLoggedIn,
+		User:     loggedInUser,
+	})
 }
 func sessionLoginHandler(c *fiber.Ctx) error {
 	return nil
