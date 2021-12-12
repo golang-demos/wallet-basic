65e4461992e126ed15e4a86383687b0c8805b871
Vinay Jeurkar <vjeurkar03@gmail.com>
2021-12-12 17:50:25 +0530

    Code  optimized

--- api/helpers/helpers.go
+++ api/helpers/helpers.go
@@ -1,18 +1,1 @@
 package helpers
-
-import (
-	"context"
-
-	"github.com/golang-demos/ecommerce-basic/database"
-	"github.com/golang-demos/ecommerce-basic/models"
-)
-
-func GetUserBySessionId(sessToken string) *models.User {
-	collection := database.Db.Collection("users")
-	ctx := context.Background()
-
-	var user models.User
-	collection.FindOne(ctx, models.User{Token: sessToken}).Decode(&user)
-
-	return &user
-}
