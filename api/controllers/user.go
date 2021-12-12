b9e0273556a2f9763b63c627a90e8fc51eedaf7a
Vinay Jeurkar <vjeurkar03@gmail.com>
2021-12-12 14:05:23 +0530

    Added User Signup feature

--- api/controllers/user.go
+++ api/controllers/user.go
@@ -2,8 +2,21 @@ package controllers
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/golang-demos/ecommerce-basic/models"
 )
 
 func userSignupHandler(c *fiber.Ctx) error {
-	return nil
+
+	var postData models.UserSignupData
+	c.BodyParser(&postData)
+
+	user := new(models.User).Init(postData)
+
+	errors := user.Validate()
+	if errors != nil {
+		return c.JSON(errors)
+	}
+
+	result := models.CreateUser(user)
+	return c.JSON(result)
 }
---
+++ api/models/userModel.go
@@ -0,0 +1,82 @@
+package models
+
+import (
+	"context"
+	"math/rand"
+
+	"crypto/md5"
+	"encoding/hex"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/golang-demos/ecommerce-basic/database"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type User struct {
+	ID       primitive.ObjectID `json:"id,omitempty" bson="_id,omitempty"`
+	Name     string             `json:"name,omitempty" validate:"required,min=2,max=32" bson="name,omitempty"`
+	Mobile   string             `json:"mobile,omitempty" validate:"required,min=2,max=32" bson="mobile,omitempty"`
+	Role     string             `json:"role,omitempty" validate:"required,min=2,max=16" bson="role,omitempty"`
+	Password string             `json:"password,omitempty" validate:"required,min=2,max=16" bson="password,omitempty"`
+	Token    string             `json:"token,omitempty" validate:"required,min=24,max=24" bson="token,omitempty"`
+}
+
+type UserSignupData struct {
+	Name     string `json:"name" validate:"required,min=2,max=32"`
+	Mobile   string `json:"mobile" validate:"required,min=2,max=32"`
+	Password string `json:"password" validate:"required,min=2,max=16"`
+}
+
+func (u *User) Init(postData UserSignupData) *User {
+	if u.Role == "" {
+		u.Role = "user"
+	}
+	if u.Token == "" {
+		u.Token = generateRandomString(24)
+	}
+	u.Name = postData.Name
+	u.Mobile = postData.Mobile
+	u.Password = postData.Password
+	return u
+}
+
+func (u *User) ToShort() *User {
+	var user User
+	user.ID = u.ID
+	user.Name = u.Name
+	user.Role = u.Role
+	return &user
+}
+
+func (u *User) Validate() []*ErrorResp {
+	return validateModelsForErrors(validator.New().Struct(u))
+}
+
+func generateRandomString(expectedLength int) string {
+	const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	b := make([]byte, expectedLength)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
+
+func checkIfExists(user *User) bool {
+	ctx := context.Background()
+	var foundUser User
+	err := database.UserCollection.FindOne(ctx, bson.D{{"mobile", user.Mobile}}).Decode(&foundUser)
+	return err == nil
+}
+
+func CreateUser(user *User) bool {
+	alreadyExists := checkIfExists(user)
+	if alreadyExists {
+		return false
+	}
+	hash := md5.Sum([]byte(user.Password))
+	user.Password = hex.EncodeToString(hash[:])
+	result, _ := database.UserCollection.InsertOne(context.Background(), user)
+	return result != nil
+}
