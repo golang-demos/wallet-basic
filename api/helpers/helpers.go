4b3d05a77b5251cfd242f697b2be5942afc2db51
Vinay Jeurkar <vjeurkar03@gmail.com>
2021-12-12 14:05:04 +0530

    Changed Db connection flow

--- api/helpers/helpers.go
+++ api/helpers/helpers.go
@@ -2,17 +2,13 @@ package helpers
 
 import (
 	"context"
-	"os"
 
 	"github.com/golang-demos/ecommerce-basic/database"
 	"github.com/golang-demos/ecommerce-basic/models"
 )
 
 func GetUserBySessionId(sessToken string) *models.User {
-	database.ConnectDB()
-	defer database.DisconnectDB()
-
-	collection := database.Client.Database(os.Getenv("ECOMM_DB_NAME")).Collection("users")
+	collection := database.Db.Collection("users")
 	ctx := context.Background()
 
 	var user models.User
