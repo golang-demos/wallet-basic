50d0e1da8f596332902d0b40ce5c4150597bef53
Vinay Jeurkar <vjeurkar03@gmail.com>
2021-12-15 01:01:21 +0530

    Added Get wallet details API

--- api/controllers/wallet.go
+++ api/controllers/wallet.go
@@ -1,11 +1,32 @@
 package controllers
 
 import (
+	"context"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/golang-demos/ecommerce-basic/database"
+	"github.com/golang-demos/ecommerce-basic/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func walletDetailsHandler(c *fiber.Ctx) error {
-	return nil
+	id := c.Params("id")
+	userObjectId, _ := primitive.ObjectIDFromHex(id)
+
+	var wallet models.Wallet
+	cursor, _ := database.WalletColllection.Find(context.Background(), bson.M{
+		"user_id": userObjectId,
+	})
+	cursor.Decode(&wallet)
+	if wallet.ID.IsZero() {
+		c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"message": "Not found",
+			"success": false,
+		})
+	}
+
+	return c.JSON(wallet)
 }
 
 func walletDepositHandler(c *fiber.Ctx) error {
