b565333381bfb527e78e95d41a854737ffd817d2
Vinay Jeurkar <vjeurkar03@gmail.com>
2021-12-16 01:39:36 +0530

    Removed omitempty

--- api/models/walletModel.go
+++ api/models/walletModel.go
@@ -10,7 +10,7 @@ import (
 )
 
 type Wallet struct {
-	ID      primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	ID      primitive.ObjectID `json:"_id" bson:"_id"`
 	UserID  primitive.ObjectID `json:"user_id" bson:"user_id,"`
 	Balance float32            `json:"balance" bson:"balance"`
 }
