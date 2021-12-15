bce89c9c3079e8b337c6f5e08f9a3c115f08b3f3
Vinay Jeurkar <vjeurkar03@gmail.com>
2021-12-16 02:30:08 +0530

    Added transaction API Handler

--- api/controllers/wallet.go
+++ api/controllers/wallet.go
@@ -21,15 +21,84 @@ func walletDetailsHandler(c *fiber.Ctx) error {
 	}).Decode(&wallet)
 
 	if wallet.ID.IsZero() {
-		c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
 			"message": "Not found",
 			"success": false,
 		})
 	}
 
-	return c.JSON(wallet)
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"success": true,
+		"balance": wallet.Balance,
+	})
 }
 
 func walletDepositHandler(c *fiber.Ctx) error {
-	return nil
+	type makeTransactionData struct {
+		TransType string  `json:"trans_type"`
+		Amount    float32 `json:"amount"`
+	}
+
+	var postData makeTransactionData
+	c.BodyParser(&postData)
+
+	UserId := c.Params("id")
+	UserObjectId, _ := primitive.ObjectIDFromHex(UserId)
+	var user models.User
+	err := database.UserCollection.FindOne(context.Background(), bson.M{
+		"_id": UserObjectId,
+	}).Decode(&user)
+	if err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"success": false,
+			"error":   "Not Found",
+		})
+	}
+
+	if postData.TransType != "credit" && postData.TransType != "debit" {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"success": false,
+			"error":   "Invalid Transaction type",
+		})
+	}
+
+	var wallet models.Wallet
+	err = database.WalletColllection.FindOne(context.Background(), bson.M{
+		"user_id": UserObjectId,
+	}).Decode(&wallet)
+	if err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"success": false,
+			"error":   "Wallet not found",
+		})
+	}
+
+	transaction, ok := models.MakeTransaction(wallet, postData.TransType, postData.Amount)
+	if !ok || transaction.ID.IsZero() {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"success": false,
+			"error":   "Failed to create transaction",
+		})
+	}
+
+	if postData.TransType == "credit" {
+		wallet.Balance = wallet.Balance + transaction.Amount
+	} else if postData.TransType == "debit" {
+		wallet.Balance = wallet.Balance - transaction.Amount
+	}
+	filter := bson.M{"_id": wallet.ID}
+	udpateData := bson.D{{"$set", bson.D{{"balance", wallet.Balance}}}}
+	_, err = database.WalletColllection.UpdateOne(context.Background(), filter, udpateData)
+
+	if err != nil {
+		// TODO: Delete transaction at this point
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"success": false,
+			"error":   "Failed to update balance",
+		})
+	}
+	return c.JSON(fiber.Map{
+		"success": true,
+		"balance": wallet.Balance,
+	})
 }
